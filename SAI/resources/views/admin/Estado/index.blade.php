0e979129ab45c7cb32ffcaf0e1e85374314d6c48
Carlos David <carlosdavidgarciab@gmail.com>
2018-02-27 20:59:17 -0400

    Agregue la vista admin.estado.index que lista los estados. funciona todo menos la paginacion

---
+++ SAI/resources/views/admin/Estado/index.blade.php
@@ -0,0 +1,40 @@
+@extends('admin.template.main')
+
+@section('title', 'Listar estados')
+
+@section('body')
+	{{-- {{ dd($estado) }} --}}
+
+	<section class="container-fluid">
+		<div class="row">
+			<div class="col-sm-8 offset-2">
+				<table class="table table-inverse">
+				  <thead>
+				    <tr>
+				      <th>ID</th>
+				      <th>Nombre del estado</th>
+				    </tr>
+				  </thead>
+				  <tbody>
+
+				  	@foreach ($estado as $estado)
+				  		<tr>
+					      <th scope="row">{{ $estado->id }}</th>
+					      <td>{{ $estado->est_nombre }}</td>	
+					      <td><a href="" class="btn btn-danger"></a> <a href="" class="btn btn-warning"></a></td>
+				    	</tr>
+				  	@endforeach
+
+				  </tbody>
+				</table>
+					
+			</div>
+			
+		</div>
+			
+	</section>
+
+
+	
+
+@endsection
