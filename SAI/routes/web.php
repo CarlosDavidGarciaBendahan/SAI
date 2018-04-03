2439789dced99b21ab52dcb818bff199bec36182
Carlos David <carlosdavidgarciab@gmail.com>
2018-04-03 19:37:23 -0400

    agregue la funcionalidad (ejemplo) para descargar el PDF en wed.php y la vista vistaPDF.blade.php

--- SAI/routes/web.php
+++ SAI/routes/web.php
@@ -371,6 +371,6 @@ Route::get('/ajax-ObtenerDatosProducto_Articulo/{Articulo_id}','Producto_Articul
 ///////////////////////////////////////////////////
 
 Route::get('PDF-ejemplo/{presupuesto_id}',function($presupuesto_id){
-	$pdf = PDF::loarView('vistaPDF');
+	$pdf = PDF::loadView('vistaPDF',['id'=> $presupuesto_id]);
 	return $pdf->download('presupuesto'.'#'.$presupuesto_id.'.pdf');
 });
