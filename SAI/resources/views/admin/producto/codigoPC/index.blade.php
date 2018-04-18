cd4e32d13428888c3c6518d5d24d37e61763d87f
Carlos David <carlosdavidgarciab@gmail.com>
2018-04-18 19:49:33 -0400

    Acomode en SAI\app\venta.php la relacion VentasPC dado que faltaba el belongsto. En codigoPC\index.blade.php ahora muestro el codigo, marca, modelo, tipo, ubicacion, si esta publicado o no y si esta disponible o no el producto.

--- SAI/resources/views/admin/producto/codigoPC/index.blade.php
+++ SAI/resources/views/admin/producto/codigoPC/index.blade.php
@@ -10,50 +10,59 @@
 		<div class="row">
 			<div class="col-sm-8 offset-2">
 
-				<!--
-				<a href="{{ route('producto_articulo.create') }}" class="btn btn-info">Registrar nuevo articulo</a>
-				-->
+				
 
 				<table class="table table-inverse">
 				  <thead>
 				    <tr>
 				      <th>Código</th>
-				      <th>Descripción</th>
-				      <th>Precio</th>
-				      <th>Publicado</th>
-				      <th>Ubicación</th>
-				      <th>Marca</th>
+				      <th>Marca/Modelo</th>
 				      <th>Tipo</th>
+				      <th>Ubicación</th>
+				      <th>Publicado?</th>
+				      <th>Disponible</th>
 
 				    </tr>
 				  </thead>
 				  <tbody>
 
-				  	@foreach ($codigosPC as $producto_articulo)
+				  	@foreach ($codigosPC as $codigoPC)
 				  		<tr>
-					      <th scope="row">{{ $producto_articulo->pro_art_codigo }}</th>
-					      <td>{{ $producto_articulo->pro_art_descripcion }}</td>	
-					      <td>{{ $producto_articulo->pro_art_precio . $producto_articulo->pro_art_moneda }}</td>
-					      <td>@if ($producto_articulo->pro_art_catalogo !== 0)
-					      	SI
-					      @else
-					      	NO
-					      @endif</td>	
-					      <td>{{ $producto_articulo->sector->sec_sector ." Ofi: ".$producto_articulo->sector->oficina->ofi_direccion }}</td>	
-					      <td>{{ $producto_articulo->modelo->marca->mar_marca . " Modelo: " .$producto_articulo->modelo->mod_modelo   }}</td>	
-					      <td>{{ $producto_articulo->tipo_producto->tip_tipo}}</td>		
+					      <th scope="row">{{ $codigoPC->cod_pc_codigo }}</th>
+					      <td>{{ "Marca: ".$codigoPC->producto_computador->modelo->marca->mar_marca ." Modelo: ".$codigoPC->producto_computador->modelo->mod_modelo }}</td>	
+					      <td>{{ $codigoPC->producto_computador->Tipo_Producto->tip_tipo }}</td>
+					      
+					      <td>{{ $codigoPC->producto_computador->sector->sec_sector ." Ofi: ".$codigoPC->producto_computador->sector->oficina->ofi_direccion }}</td>	
 					      <td>
-					      	<a href="{{ route('producto_articulo.edit', $producto_articulo->id) }}" class="btn btn-warning">
+					      	@if ($codigoPC->producto_computador->pro_com_catalogo !== 0)
+					      		SI
+						    @else
+						      	NO
+						    @endif
+					  	  </td>	
+					  	  <td>
+					  	  	@if (count($codigoPC->Solicitudes) === 0 && count($codigoPC->Ventas) === 0  )
+					  	  		<a href="#" class="btn btn-success" title="Disponible">
+					      		<span class="class glyphicon glyphicon-ok"></span>
+					      		</a>
+					  	  	@else
+					  	  		<a href="#" class="btn btn-danger" title="Vendido">
+					      		<span class="class glyphicon glyphicon-ban-circle"></span>
+					      		</a>
+					  	  	@endif
+					  	  </td>
+					      <td>
+					      	<a href="{{ route('codigoPC.edit', $codigoPC->id) }}" class="btn btn-warning">
 					      		<span class="class glyphicon glyphicon-wrench"></span>
 					      	</a>
 
-					      	<a href="{{ route('producto_articulo.destroy', $producto_articulo->id) }}" onclick="return confirm('Eliminar el producto_articulo?')" class="btn btn-danger">
+					      	<a href="{{ route('codigoPC.destroy', $codigoPC->id) }}" onclick="return confirm('Eliminar el codigoPC?')" class="btn btn-danger">
 					      		<span class="class glyphicon glyphicon-remove-circle"></span>
 					      	</a> 
-					      	<a href="{{ route('producto_articulo.show', $producto_articulo->id) }}" class="btn btn-info">
+					      	<a href="{{ route('codigoPC.show', $codigoPC->id) }}" class="btn btn-info">
 					      		<span class="glyphicon glyphicon-search"></span>
 					      	</a>
-					      	<a href="{{ route('codigoArticulo.create', $producto_articulo->id) }}" class="btn btn-success" title="Agregar Articulos">
+					      	<a href="{{ route('codigoArticulo.create', $codigoPC->id) }}" class="btn btn-success" title="Agregar Articulos">
 					      		<span class="glyphicon glyphicon-plus-sign"></span>
 					      	</a>
 					      </td>
