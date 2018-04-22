3830b2940b6c2d61f798443d8350535f394c8949
Carlos David <carlosdavidgarciab@gmail.com>
2018-04-22 16:54:52 -0400

    Ha sido modificado el composer.json y se agrego el provider de maatwebsite/excel, como tambien fue agregado el alias y la clase para manejo del excel

--- SAI/config/app.php
+++ SAI/config/app.php
@@ -166,6 +166,7 @@ return [
         Collective\Html\HtmlServiceProvider::class,
         Laracasts\Flash\FlashServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
         //Laracasts\Utilities\JavaScript\JavaScriptServiceProvider::class,
         
 
@@ -223,6 +224,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'PDF' => Barryvdh\DomPDF\Facade::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     ],
 
 ];
