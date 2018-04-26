0893e18e8891e92d5073cc30569ea1c4c2636e10
Carlos David <carlosdavidgarciab@gmail.com>
2018-04-26 13:29:16 -0400

    Se creo la tabla cambio_bolivar, donde tendra el cambio actual y la fecha de ee tipo de cambio. Se modifico el ER y se agrego la migracion

---
+++ SAI/database/migrations/2018_04_26_172241_create_table__cambio__bolivar.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableCambioBolivar extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Cambio_Bolivar', function (Blueprint $table) {
+            $table->increments('id');
+            $table->decimal('cam_cantidad',9,2)->nullable(false);
+            $table->date('cam_fecha')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Cambio_Bolivar');
+    }
+}
