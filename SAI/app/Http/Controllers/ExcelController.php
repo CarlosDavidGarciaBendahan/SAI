1b96a4e0f216d7242507dfbc2f99585245b71976
Carlos David <carlosdavidgarciab@gmail.com>
2018-06-09 09:24:06 -0400

    Ya puedo agarrar la primera fila del excel y obtener todos los resultados siguientes de 1 columna especifica

--- SAI/app/Http/Controllers/ExcelController.php
+++ SAI/app/Http/Controllers/ExcelController.php
@@ -21,19 +21,29 @@ class ExcelController extends Controller
     		//agarro los resultados..
     		$resultados = $archivo->get($columnas);
     		//dd($resultados->all());
-    		foreach ($resultados as $resultado) {
-    			echo 	$resultado->fecha_pagado."<br>".
-    					$resultado->monto." ".
-    					$resultado->moneda."<br>".
-    					$resultado->concepto."<br>".
-    					$resultado->forma."<br>".
-    					$resultado->numero_referencia."<br>".
-    					$resultado->banco_origen."<br>".
-    					$resultado->banco_destino."<br>".
-    					$resultado->numero_venta."<br>"
-    			;
-    		}
 
+    		echo 	$resultados[0]->fecha_pagado." --- ".
+    				$resultados[0]->monto." --- ".
+    				$resultados[0]->moneda." --- ".
+    				$resultados[0]->concepto." --- ".
+    				$resultados[0]->forma." --- ".
+    				$resultados[0]->numero_referencia." --- ".
+    				$resultados[0]->banco_origen." --- ".
+    				$resultados[0]->banco_destino." --- ".
+    				$resultados[0]->numero_venta."<br>";
+    		foreach ($resultados as $resultado) {
+    			echo 	$resultado->fecha_pagado."<br>";
+    					/*$resultado->monto." --- ".
+    					$resultado->moneda." --- ".
+    					$resultado->concepto." --- ".
+    					$resultado->forma." --- ".
+    					$resultado->numero_referencia." --- ".
+    					$resultado->banco_origen." --- ".
+    					$resultado->banco_destino." --- ".
+    					$resultado->numero_venta."<br>"
+    			;*/
+    			//break;//ejecuto este codigo una sola vez y lo rompo
+    		}
     	})->get();
 
     }
